52a2f938a9a2fc63d1f06b977df63b6fdca5bf82
anti-pc <buketculfaoglu@gmail.com>
2024-03-21 18:37:50 +0300

    cleanup

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -80,56 +80,5 @@ namespace WebApi.Controllers
             return Ok();           
         }
 
-        // private static List<Book> BookList = new List<Book>()
-        // {
-        //     new Book
-        //     {
-        //         Id=1,
-        //         Title="Lean Startup",
-        //         GenreId=1,
-        //         PageCount=200,
-        //         PublishDate=new DateTime(2021,06,12)
-        //     },
-
-        //     new Book{
-        //         Id=2,
-        //         Title="Herland",
-        //         GenreId=2,
-        //         PageCount=250,
-        //         PublishDate=new DateTime(2021,05,23)
-        //     },
-
-        //     new Book{
-        //         Id=3,
-        //         Title="Dune",
-        //         GenreId=2,
-        //         PageCount=540,
-        //         PublishDate=new DateTime(2021,12,21)
-        //     }
-        // };
-
-        // [HttpGet]
-        // public List<Book> GetBooks()
-        // {
-        //     var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
-        //     return bookList;
-        // }
-
-        // [HttpGet("{id}")]
-        // public Book GetById(int id)
-        // {
-        //     var book = BookList.Where(x => x.Id == id).SingleOrDefault();
-        //     return book;
-        // }
-
-
-        // [HttpGet]
-        // public Book Get([FromQuery] string id)
-        // {
-        //     var book = BookList.Where(x => x.Id == Convert.ToInt32(id)).SingleOrDefault();
-        //     return book;
-        // }
-
-
     }
 }
